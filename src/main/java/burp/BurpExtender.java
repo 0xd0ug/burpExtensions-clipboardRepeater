52b8c6114f48ab45b7d56ddb1c97fed6184711cd
Doug <noone@localhost>
2021-01-14 11:37:48 -0500

    Create README and update extension name.

--- src/main/java/burp/BurpExtender.java
+++ src/main/java/burp/BurpExtender.java
@@ -5,12 +5,12 @@ public class BurpExtender implements IBurpExtender
     public void registerExtenderCallbacks (IBurpExtenderCallbacks callbacks)
     {
 
-        callbacks.setExtensionName ("Clipboard Repeater");
+        callbacks.setExtensionName ("RepeaterClips");
         callbacks.registerContextMenuFactory(new RCMenu(callbacks));
     }
 
     public static void main(String[] args) {
 
-        System.out.println("Clipboard Repeater Burp Extension");
+        System.out.println("RepeaterClips Burp Extension");
     }
 }
